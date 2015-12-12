c2db5e190040740767a1019da0af0f5ce826ae47
Jeff Handley <jeff.handley@concur.com>
2015-12-11 17:09:58 -0800

    Hello World with babel and express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+let server = app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
